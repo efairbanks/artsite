34d7229a711dc359f2e88e16807005fbadbdb8a1
Eric Fairbanks <eric.p.fairbanks@gmail.com>
2013-11-06 16:56:17 -0500

    Added admin page, fades at top and bottom of page, static nav, etc...

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,12 +1,10 @@
 class SiteController < ApplicationController
-  
-  skip_before_filter :authenticate
-
-  layout 'site.html.erb'
-
   def home
     @home_page  = Page.where(title: params[:title] || 'Home')
     @root_pages = Page.where(root_page: true)
     @child_pages = @home_page.first.pages.all
+    if @child_pages.empty? and not @home_page.first.page_id.nil? and not @home_page.first.root_page
+      @child_pages = Page.where(id: @home_page.first.page_id).first.pages.all
+    end
   end
 end
